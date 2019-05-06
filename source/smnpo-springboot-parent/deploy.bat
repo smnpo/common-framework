ace2bab24c206c1c8970004a945dc77ef9a98ad0
snaildev <snailtem@gmail.com>
2019-05-06 15:35:00 +0800

    0.0.1

--- source/smnpo-springboot-parent/deploy.bat
+++ source/smnpo-springboot-parent/deploy.bat
@@ -1,3 +1,3 @@
 set VERSION=1.0-SNAPSHOT
-set PROJECT=common-framework
+set PROJECT=smnpo-springboot-parent
 mvn install deploy:deploy-file -DgroupId=io.github.smnpo -DartifactId=%PROJECT% -Dversion=%VERSION% -Dpackaging=pom -Dfile=.\target\%PROJECT%-%VERSION%.pom -Durl=http://maven.develophelper.com/repository/maven-snapshots/ -DrepositoryId=personalsnapshots
--- source/smnpo-springboot-web-starter/src/main/java/io/github/smnpo/Application.java
+++
@@ -1,9 +0,0 @@
-package io.github.smnpo;
-
-/**
- * @author: Ming.Zhao
- * @create: 2019-05-05 18:10
- **/
-public class Application {
-
-}

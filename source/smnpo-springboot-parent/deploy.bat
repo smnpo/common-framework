985fdd29bb166cb26a4127fd37bf579a977b97ef
snaildev <snailtem@gmail.com>
2019-05-09 11:47:02 +0800

    maven repository 切换成云效

--- source/smnpo-springboot-parent/deploy.bat
+++ source/smnpo-springboot-parent/deploy.bat
@@ -1,3 +1,4 @@
 set VERSION=1.0-SNAPSHOT
 set PROJECT=smnpo-springboot-parent
-mvn install deploy:deploy-file -DgroupId=io.github.smnpo -DartifactId=%PROJECT% -Dversion=%VERSION% -Dpackaging=pom -Dfile=.\target\%PROJECT%-%VERSION%.pom -Durl=http://maven.develophelper.com/repository/maven-snapshots/ -DrepositoryId=personalsnapshots
+set URL=https://repo.rdc.aliyun.com/repository/71943-snapshot-gMBvZN/
+mvn install deploy:deploy-file -DgroupId=io.github.smnpo -DartifactId=%PROJECT% -Dversion=%VERSION% -Dpackaging=pom -Dfile=.\target\%PROJECT%-%VERSION%.pom -Durl=%URL% -DrepositoryId=rdc-snapshots
--- source/smnpo-swagger-starter/deploy.bat
+++ source/smnpo-swagger-starter/deploy.bat
@@ -1,3 +1,4 @@
 set VERSION=1.0-SNAPSHOT
 set PROJECT=smnpo-swagger-starter
-mvn clean install deploy:deploy-file -DgroupId=io.github.smnpo -DartifactId=%PROJECT% -Dversion=%VERSION% -Dpackaging=jar -Dfile=.\target\%PROJECT%-%VERSION%.jar -Durl=http://maven.develophelper.com/repository/maven-snapshots/ -DrepositoryId=personalsnapshots
+set URL=https://repo.rdc.aliyun.com/repository/71943-snapshot-gMBvZN/
+mvn clean install deploy:deploy-file -DgroupId=io.github.smnpo -DartifactId=%PROJECT% -Dversion=%VERSION% -Dpackaging=jar -Dfile=.\target\%PROJECT%-%VERSION%.jar -Durl=%URL% -DrepositoryId=rdc-snapshots

c4818fc227672ee4c620ce827f9d188504a66010
snaildev <snailtem@gmail.com>
2019-05-11 17:33:06 +0800

    spi

--- source/smnpo-springboot-parent/deploy.bat
+++
@@ -1,4 +0,0 @@
-set VERSION=0.0.1
-set PROJECT=smnpo-springboot-parent
-set URL=https://repo.rdc.aliyun.com/repository/71943-release-LWfjuf/
-mvn install deploy:deploy-file -DgroupId=io.github.smnpo -DartifactId=%PROJECT% -Dversion=%VERSION% -Dpackaging=pom -Dfile=.\target\%PROJECT%-%VERSION%.pom -Durl=%URL% -DrepositoryId=rdc-releases

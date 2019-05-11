b9ef2aca2fb944e6fba7862ae1dd8504517d5c4d
snaildev <snailtem@gmail.com>
2019-05-11 18:17:34 +0800

    remove

--- source/smnpo-common/deploy.bat
+++
@@ -1,4 +0,0 @@
-set VERSION=0.0.1
-set PROJECT=smnpo-common
-set URL=https://repo.rdc.aliyun.com/repository/71943-release-LWfjuf/
-mvn clean install deploy:deploy-file -DgroupId=io.github.smnpo -DartifactId=%PROJECT% -Dversion=%VERSION% -Dpackaging=jar -Dfile=.\target\%PROJECT%-%VERSION%.jar -Durl=%URL% -DrepositoryId=rdc-releases
